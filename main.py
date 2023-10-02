569ab8f4942a3c22d6d00d34c5c95fe2035716b9
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2023-10-02 20:05:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+input_string = input("Введіть рядок: ")
+
+
+
+if input_string == input_string[::-1]:
+
+ print("Введений рядок є паліндромом")
+
+else:
+
+ print("Введений рядок не є паліндромом")
+
